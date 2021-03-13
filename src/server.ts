507262c5c25299427fc21413bfc34894b158057a
Pedro Gomez <pedrodeveloper14@gmail.com>
2021-03-13 12:47:03 -0600

    project init

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import Routes from './routes/index';
+
+const app: express.Application = express();
+
+
+app.use(Routes);
+
+app.listen(3000, () => {
+    console.log("listening at 3000");
+})
